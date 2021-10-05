c9a4647ceb4d812ea569591282b6bab70cf9d08b
juvensius96 <juvensius96@gmail.com>
2021-10-05 11:49:14 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+  $("button").click(function(){
+    $("p").hide("slow", function(){
+      alert("Berhasil Logout!");
+    });
+  });
+});
+</script>
+</head>
+<body>
+
+<button class="btn btn-danger">Logout</button>
+
+<p></p>
+
+</body>
+</html>
